2e15841640e39205b37569171362f9d23d6eedb1
zxf123 <xiangfuz@cn.ibm.com>
2024-01-24 15:31:58 +0800

    order-server first commit

---
+++ src/main/java/com/example/orderserver/OrderServerApplication.java
@@ -0,0 +1,19 @@
+package com.example.orderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+@EnableFeignClients(basePackages = {"com.example.orderserver.feignClient"})
+public class OrderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/orderserver/feignClient/IProductFeignClient.java
@@ -0,0 +1,15 @@
+package com.example.orderserver.feignClient;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "product-server")
+public interface IProductFeignClient {
+
+    @GetMapping("/productTestContraller/updateStore")
+    String updateStore(@RequestParam("productNo") String productNo);
+}
